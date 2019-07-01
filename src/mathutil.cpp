3ad3bc4f6ada824fa59aa1ffcd623737c2b207a0
QueensGambit <curry-berry@freenet.de>
2019-07-01 16:27:58 +0200

    enabled flexible number of executors

---
+++ src/mathutil.cpp
@@ -0,0 +1,23 @@
+/*
+ * CrazyAra, a deep learning chess variant engine
+ * Copyright (C) 2018 Johannes Czech, Moritz Willig, Alena Beyer
+ * Copyright (C) 2019 Johannes Czech
+ *
+ * CrazyAra is free software: You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @file: mathutil.cpp
+ * Created on 01.07.2019
+ * @author: queensgambit
+ *
+ * Please describe what the content of this file is about
+ */
+
+#include "mathutil.h"
+
+MathUtil::MathUtil()
+{
+
+}
---
+++ src/mathutil.h
@@ -0,0 +1,28 @@
+/*
+ * CrazyAra, a deep learning chess variant engine
+ * Copyright (C) 2018 Johannes Czech, Moritz Willig, Alena Beyer
+ * Copyright (C) 2019 Johannes Czech
+ *
+ * CrazyAra is free software: You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @file: mathutil.h
+ * Created on 01.07.2019
+ * @author: queensgambit
+ *
+ * Please describe what the content of this file is about
+ */
+
+#ifndef MATHUTIL_H
+#define MATHUTIL_H
+
+
+class MathUtil
+{
+public:
+    MathUtil();
+};
+
+#endif // MATHUTIL_H
